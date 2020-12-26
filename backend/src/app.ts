270ba139578e40131588fba6b81f9a8ccfbe82dd
Lucas Cazeto <lucas.cazeto@klarna.com>
2020-12-26 15:03:24 +0100

    initial setup

---
+++ backend/src/app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import * as bodyParser from "body-parser";
+
+const app = express();
+
+app.use(
+  bodyParser.json({
+    limit: "1mb",
+    verify(req: any, res, buf, encoding) {
+      req.rawBody = buf;
+    },
+  })
+);
+
+app.get("/", (req, res) => res.send("Hello World!"));
+export { app };
