592da16941e4126b7e42bdc78a357e8a7b5c996a
Lucas Cazeto <lucas.cazeto@klarna.com>
2020-12-31 23:25:10 +0100

    BE: using dotenv

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,8 @@
 import { app } from "./src/app";
 import { AddressInfo } from "net";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const server = app.listen(5000, "localhost", () => {
   const { port, address } = server.address() as AddressInfo;
